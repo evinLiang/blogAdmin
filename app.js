45d6ce5acd8f783c92070e4d8e7bfa2c8286c615
evinLiang <evin@evinweb.com>
2018-07-19 17:57:51 +0800

    新增用户、图片库路由

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var articleRouter = require('./routes/article');
+var photoRouter = require('./routes/photo');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/article', articleRouter);
+app.use('/photo', photoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/photo.js
+++ routes/photo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: '用户信息' });
+  res.render('photo', { title: '图片仓库' });
 });
 
 module.exports = router;
