488a21b4eb44ced3b7eea851ed2729b8f6428396
evinLiang <evin@evinweb.com>
2018-07-23 18:37:56 +0800

    oss

--- routes/photo.js
+++ routes/photo.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('photo', { title: '图片仓库' });
 });
 
+// 上传图片 add ==> post
+router.post('/add', function(req, res, next) {
+	console.log(1);
+  res.send(1);
+});
+
 module.exports = router;
